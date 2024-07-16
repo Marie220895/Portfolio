fcade48d0a64c47bf909eb03da0ab74e362e94e5
Marie <Marie@MacBook-Pro-de-DUBUS.local>
2024-07-16 22:08:33 +0200

    Initial commit

---
+++ src/components/data/menuData.js
@@ -0,0 +1,12 @@
+export const mainMenu = [
+	{ link: '#', content: 'Accueil', icon: 'lucide:home' },
+	{ link: '#about', content: 'A propos', icon: 'octicon:person-24' },
+	{ link: '#skills', content: 'Compétences', icon: 'carbon:portfolio' },
+	{ link: '#portfolio', content: 'Portfolio', icon: 'carbon:portfolio' },
+	// { link: '#blog', content: 'Blog', icon: 'mdi:blog' },
+];
+// export const menuBlog = [
+// 	{ link: '/', content: 'Home', icon: 'lucide:home' },
+// 	{ link: '/#about', content: 'About', icon: 'octicon:person-24' },
+// 	{ link: '/#portafolio', content: 'Portfolio', icon: 'carbon:portfolio' },
+// ];
