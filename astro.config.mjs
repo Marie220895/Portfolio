2030463abb53d56fd4b085687a1b2ea4dbeae8e4
Marie <Marie@MacBook-Pro-de-DUBUS.local>
2024-08-05 12:05:35 +0200

    add file config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false
-  },
-  integrations: [react(), tailwind(), sitemap()]
+	site: 'https://github.com/Marie220895/Portfolio',
+	// base: '/porfolio',
+
+	devToolbar: {
+		enabled: false,
+	},
+	integrations: [react(), tailwind(), sitemap()],
 });
