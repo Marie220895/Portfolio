2466c89feaf9c3a846a40724eccd2931dc46f1c8
Marie <marie.taillard2208@gmail.com>
2024-08-05 15:22:57 +0200

    modification astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://github.com/Marie220895/Portfolio',
+	site: 'https://Marie220895.github.io',
 	base: 'Porfolio',
 
 	devToolbar: {
