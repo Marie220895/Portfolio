b461726e3ed12595701618b5c66c4e83baf4ed8b
Marie <marie.taillard2208@gmail.com>
2024-08-05 15:33:04 +0200

    modification astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Marie220895.github.io',
-	base: 'Porfolio',
+	base: '/Porfolio',
 
-	devToolbar: {
-		enabled: false,
-	},
+	// devToolbar: {
+	// 	enabled: false,
+	// },
 	integrations: [react(), tailwind(), sitemap()],
 });
